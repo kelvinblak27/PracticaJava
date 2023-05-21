d0777c8bfd52390cc8bd8edd11a8e65b76b6495b
kelvinblak27 <kelvinblak16@gmail.com>
2023-05-21 19:00:15 -0300

    Mi primer commit

---
+++ primer curso eclipsejava/src/variables.java
@@ -0,0 +1,23 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+
+		int edad; // iniciacion de variable en unica linea
+		
+		edad=89;
+		
+		/*la siguiente inruccion imprime la consola (system.out) el valor de la variable edad
+		 * todo perefecto
+		 */
+		
+		System.out.println(edad);
+		
+		edad=35;
+				
+		System.out.println(edad);
+		
+		
+	}
+
+}
